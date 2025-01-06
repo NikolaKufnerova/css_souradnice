eefc270fc2a8a63770cde6d5898476194b0f52ec
NikolaKufnerova <146720348+NikolaKufnerova@users.noreply.github.com>
2025-01-06 08:52:46 +0100

    Add files via upload

---
+++ css_souradnice/css_souradnice/Coord.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace css_souradnice
+{
+    public class Coord : IEquatable<Coord>, IComparable<Coord>
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coord(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Size
+        {
+            get { return Math.Sqrt(X * X + Y * Y); }
+        }
+
+        public double Magnitude
+        {
+            get { return X * X + Y * Y; }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Coord other)
+            {
+                return this.X == other.X && this.Y == other.Y;
+            }
+            return false;
+        }
+
+        public bool Equals(Coord other)
+        {
+            if (other == null) return false;
+            return this.X == other.X && this.Y == other.Y;
+        }
+
+        public static bool operator ==(Coord c1, Coord c2)
+        {
+            if (ReferenceEquals(c1, null) && ReferenceEquals(c2, null)) return true;
+            if (ReferenceEquals(c1, null) || ReferenceEquals(c2, null)) return false;
+            return c1.Equals(c2);
+        }
+
+        public static bool operator !=(Coord c1, Coord c2)
+        {
+            return !(c1 == c2);
+        }
+
+        public static bool operator <(Coord c1, Coord c2)
+        {
+            return c1.Magnitude < c2.Magnitude;
+        }
+
+        public static bool operator >(Coord c1, Coord c2)
+        {
+            return c1.Magnitude > c2.Magnitude;
+        }
+
+        public int CompareTo(Coord other)
+        {
+            if (other == null) return 1;
+            return this.Magnitude.CompareTo(other.Magnitude);
+        }
+
+        public static Coord operator +(Coord c1, Coord c2)
+        {
+            return new Coord(c1.X + c2.X, c1.Y + c2.Y);
+        }
+
+        public static Coord operator *(Coord c, int scalar)
+        {
+            return new Coord(c.X * scalar, c.Y * scalar);
+        }
+
+        public static Coord operator *(int scalar, Coord c)
+        {
+            return new Coord(c.X * scalar, c.Y * scalar);
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+
+        public override int GetHashCode()
+        {
+            return X.GetHashCode() ^ Y.GetHashCode();
+        }
+    }
+}
---
+++ css_souradnice/css_souradnice/Program.cs
@@ -0,0 +1,22 @@
+﻿using css_souradnice;
+
+Coord c1 = new Coord(10, 10);
+Coord c2 = new Coord(10, 10);
+Coord c3 = new Coord(5, 5);
+Coord c4 = new Coord(2, 3);
+
+Console.WriteLine(c1 == c2);
+Console.WriteLine(c1 != c3);
+Console.WriteLine(c1.Equals(c2));
+Console.WriteLine(c3.Equals(c4));
+
+Console.WriteLine(c1 > c3);
+Console.WriteLine(c3 < c4);
+
+Coord c5 = c1 + c3;
+Console.WriteLine(c5);
+
+Coord c6 = c1 * 2;
+Console.WriteLine(c6);
+
+Console.WriteLine(c1.Size);
